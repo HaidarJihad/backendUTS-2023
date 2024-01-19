40f11f300781ffb0a22e6f7019a051146a44e67f
HaidarJihad <115534998+HaidarJihad@users.noreply.github.com>
2024-01-19 08:16:44 +0700

    Delete java directory

--- java/Praktik5/Test.java
+++
@@ -1,9 +0,0 @@
-package Praktik5;
-public class Test {
-    public static void main(String args[])
-    {
-
-        MountainBike mb = new MountainBike(3, 100, 25);
-        System.out.println(mb.toString());
-    }
-}
